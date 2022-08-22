3b6a87ffece20c21d350ecd5eb720748bdea85fa
sufian <sufian4se@gmail.com>
2022-08-22 13:06:25 +0600

    button disabled

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-function addToSelect() {
-    console.log('select button clicked')
+function addToSelect(element) {
+    // console.log('select button clicked');
+    const playerName = (element.parentNode.children[1].innerText);
+    const disabledButton = element.parentNode.children[3];
+    disabledButton.setAttribute('disabled', true);
+    console.log(playerName)
+    
 }

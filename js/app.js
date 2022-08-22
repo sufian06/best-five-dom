68de05e8993d9b005db66f612297a9ed1bd0f8e9
sufian <sufian4se@gmail.com>
2022-08-22 14:43:35 +0600

    player list added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,39 @@
+const palyerArr = [];
+
+
+
+function display(player) {
+    console.log(player);
+    const playerList = document.getElementById('player-list');
+    playerList.innerHTML = '';
+
+    for(let i = 0; i < player.length; i++) {
+        const name = player[i].playerName;
+        console.log(name)
+
+        const ol = document.createElement('ol');
+        ol.innerHTML = `<li class="text-gray-300 text-lg">${i + 1}. ${name}</li>`
+        playerList.appendChild(ol);
+        
+    }
+    
+}
+
 function addToSelect(element) {
-    // console.log('select button clicked');
-    const playerName = (element.parentNode.children[1].innerText);
+    // button disable
     const disabledButton = element.parentNode.children[3];
     disabledButton.setAttribute('disabled', true);
-    console.log(playerName)
+
+    const playerName = element.parentNode.children[1].innerText;
+    const playerObj = {
+        playerName: playerName
+    }
     
+    palyerArr.push(playerObj);
+
+    display(palyerArr);
+
+    
+
+
 }
